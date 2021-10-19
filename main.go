1ae9cb406732a5b671dee68ad50fffcc2f5e7b05
Elian Van Cutsem <hello@elian.codes>
2021-10-20 00:01:31 +0200

    ♻️ add image as a struct

--- main.go
+++ main.go
@@ -19,15 +19,19 @@ type size struct {
 	height int
 }
 
-func main() {
-	var defaultSocialSize size = size{width: 2024, height: 1012}
-	generateImage("test", defaultSocialSize)
+type socialImage struct {
+	size      size
+	name      string
+	baseColor color.Color
+	src       image.Image
 }
 
-func generateImage(imageName string, size size) {
-	// initialize a new image
-	img := image.NewRGBA(image.Rect(0, 0, size.width, size.height))
+func main() {
+	var defaultSocialImageSize size = size{width: 2024, height: 1012}
+	generateImage("test", defaultSocialImageSize)
+}
 
+func getRandomColor() color.Color {
 	// Colors are defined by Red, Green, Blue, Alpha uint8 values.
 	availableColors := [5]color.Color{
 		color.RGBA{253, 186, 116, 0xff}, // ElianCodes Orange
@@ -36,11 +40,23 @@ func generateImage(imageName string, size size) {
 		color.RGBA{252, 165, 165, 0xff}, // ElianCodes Red
 		color.RGBA{240, 171, 252, 0xff}, // ElianCodes Purple
 	}
+	fmt.Println("The color is picked!")
+	return availableColors[rand.Intn(len(availableColors))]
+}
 
-	usedColor := availableColors[rand.Intn(len(availableColors))]
+func generateImage(imageName string, size size) {
+	fmt.Println("starting generation of " + imageName)
+
+	// initialize a new (empty) image
+	img := image.NewRGBA(image.Rect(0, 0, size.width, size.height))
+
+	usedColor := getRandomColor()
+	uniformColor := image.NewUniform(usedColor)
+
+	var socialImg socialImage = socialImage{size: size, name: imageName, baseColor: usedColor, src: img}
 
 	// Draw the background
-	draw.Draw(img, image.Rect(size.width/2, 0, size.width, size.height), &image.Uniform{usedColor}, image.Point{}, draw.Src)
+	draw.Draw(img, image.Rect(size.width/2, 0, size.width, size.height), uniformColor, image.Point{}, draw.Src)
 	draw.Draw(img, image.Rect(0, 0, size.width/2, size.height), &image.Uniform{color.White}, image.Point{}, draw.Src)
 
 	// Set the ElianCodes image in place
@@ -62,7 +78,7 @@ func generateImage(imageName string, size size) {
 	ctx.SetClip(img.Bounds())
 	fontBytes, err := ioutil.ReadFile("./fonts/Rubik/static/Rubik-Regular.ttf")
 	if err != nil {
-		fmt.Errorf(err.Error())
+		fmt.Println("ohoh, I encountered an error while fetching the font: " + err.Error())
 	}
 	font, err := freetype.ParseFont(fontBytes)
 	ctx.SetFont(font)
@@ -70,7 +86,12 @@ func generateImage(imageName string, size size) {
 	ctx.SetSrc(image.NewUniform(usedColor))
 	ctx.DrawString("Elian Codes", freetype.Pt(0, 0+int(ctx.PointToFixed(42)>>6)))
 
+	writeImage(socialImg)
+}
+
+func writeImage(img socialImage) {
 	// Encode as PNG.
-	f, _ := os.Create(imageName + ".png")
-	png.Encode(f, img)
+	f, _ := os.Create(img.name + ".png")
+	png.Encode(f, img.src)
+	fmt.Println("The " + img.name + " image is ready!")
 }
